0b1c75e89dc1144a0a5c512444327ab820a21cb3
nisino25 <nisino25@gmail.com>
2021-12-08 23:07:07 +0900

    first commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <iframe src="http://blsetup.city.kyoto.jp/blsp/show.php?sid=dc9ac6483ac0b58d2d5f6509836ebac6" frameborder="0"></iframe>
+  <iframe src="http://blsetup.city.kyoto.jp/blsp/show.php?sid=7c94d59cbb11f15cc7eae06842607349" frameborder="0"></iframe>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+}
+</script>
+
+<style>
+iframe{
+  width: 90%;
+  height: 40vh;
+}
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  /* margin-top: 60px; */
+}
+</style>
